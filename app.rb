4ee5d3fe3ef2ccfc634fb827eaa2dd3e19d1f4f3
xwang146 <xwang146@binghamton.edu>
2016-02-14 15:53:33 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,6 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+=begin    retval=@game.guess(letter)
+    if retval==false 
+      falsh[:message]="You have already used that letter."
+=end    end
+    
     redirect '/show'
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,13 +3,20 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
+  attr_accessor:word
+  attr_accessor:guesses
+  attr_accessor:wrong_guesses
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  #def initialize()
+    
+ # end
+
   
   def initialize(word)
     @word = word
+    @guesses= ''
+    @wrong_guesses=''
   end
 
   def self.get_random_word
@@ -18,5 +25,101 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  
+    #find if the letter has already been guessed
+      #if guessed, ignore
+      #else, check if it is correct
+      # add to list
+      
+  
+  
+  def guess(letter_guess)
+    existed=false
+    correct=false
+    #check if valid
+    
+    if (letter_guess=='') || (letter_guess==nil) || (/[a-zA-Z]/.match(letter_guess)==nil)
+      raise ArgumentError.new("invalid input")
+    end
+   
+   letter_guess.downcase!
+     if !@guesses.empty?
+       @guesses.each_char do |c|
+            if c==letter_guess
+              existed=true
+              break
+            end
+          end
+     end
+          
+          
+      if existed ==false
+        if !@wrong_guesses.empty?
+         @wrong_guesses.each_char do |c|
+            if c==letter_guess
+              existed=true
+              break
+            end
+          end
+        end
+      end
+        
+        #check if correct
+        if existed ==false
+           @word.each_char do |letter_correct|
+             if letter_guess==letter_correct
+               @guesses=@guesses+letter_guess.to_s
+               correct=true
+               break
+             end
+           end
+           
+           if correct==false
+             @wrong_guesses=@wrong_guesses+letter_guess.to_s
+           end
+        else
+          return false
+        end
+        
+  end
+  
+=begin
+word = banana
+@test_cases = {
+      'bn' =>  'b-n-n-',
+      'def' => '------',
+      'ban' => 'banana'
+    }
+=end
+  def word_with_guesses
+    retval=""
+    @word.each_char do |x|
+      if @guesses.include?(x)
+        retval=retval+x
+      else
+        retval=retval+"-"
+      end
+    end
+    return retval
+  end
+  
+  def check_win_or_lose
+    
+    if @guesses.split('').sort.join('')==@word.split('').sort.join('')
+      return :win
+    end
+    
+    if wrong_guesses.length==7
+      return :lose
+    end
+    
+    return :play
+      
+  end
+  
 
+ 
+  
 end
+#let me try run
